de46be22ef760a111255d62a7c01d88e036f0790
wazazhang <wazazhang@gmail.com>
2016-08-18 16:55:22 +0800

    Add files via upload
    Add ProtoCommentAttribute for mark code gen comments

---
+++ protobuf-net/ServiceModel/ProtoCommentAttribute.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ComponentModel;
+
+namespace ProtoBuf.ServiceModel
+{
+    [AttributeUsage(AttributeTargets.All, AllowMultiple = true)]
+    public class ProtoCommentAttribute : Attribute
+    {
+        public ProtoCommentAttribute(string description)
+        {
+            Description = description;
+        }
+        public string Description { get; private set; }
+    }
+}
